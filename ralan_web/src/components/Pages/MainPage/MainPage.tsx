de84a75fe1656f8ab9a63691ce9ad7d4bc56fcb0
denisov <denisov@itopcase.ru>
2025-09-24 22:52:56 +0300

    delete artifacts

--- ralan_web/src/components/Pages/MainPage/MainPage.tsx
+++ ralan_web/src/components/Pages/MainPage/MainPage.tsx
@@ -2,13 +2,10 @@ import Background from "@/components/Background/Background";
 import FirstSlide from "./FirstSlide/FirstSlide";
 import AboutUsSlide from "./AboutUsSlide/AboutUsSlide"
 
-import styles from "./MainPage.module.scss"
-
 export default function MainPage() {
 
   const mainBackgroundProps = {
-    backgroundColor: '#1A2344', // Пример: другой цвет
-    // imageUrl: '/images/your-awesome-background.jpg', // Пример: другое изображение
+    backgroundColor: '#1A2344',
   };
 
   return (
