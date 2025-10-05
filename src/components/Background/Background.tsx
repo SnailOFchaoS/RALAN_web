b28661311f399d8267348e52b425e8d8b7076963
denisov <denisov@itopcase.ru>
2025-10-06 01:08:43 +0300

    need fix

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -6,15 +6,17 @@ interface BackgroundProps {
 }
 
 const Background = ({ imageUrl, backgroundColor }: BackgroundProps) => {
-  const backgroundStyles: React.CSSProperties = {
-    backgroundColor: backgroundColor || '#1A2344', // Цвет по умолчанию, если не задан
-    backgroundImage: imageUrl ? `url(${imageUrl})` : 'none',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-  };
+  // const backgroundStyles: React.CSSProperties = {
+  //   backgroundColor: backgroundColor || '#1A2344', // Цвет по умолчанию, если не задан
+  //   backgroundImage: imageUrl ? `url(${imageUrl})` : 'none',
+  //   backgroundSize: 'cover',
+  //   backgroundPosition: 'center',
+  // };
+
+  // style={backgroundStyles}
 
   return (
-    <div className={styles.fixedBackground} style={backgroundStyles}>
+    <div className={styles.fixedBackground} >
 
     </div>
   );
